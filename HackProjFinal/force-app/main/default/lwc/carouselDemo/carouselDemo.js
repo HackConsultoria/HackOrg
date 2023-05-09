d77630d63e0693f25ce8447b4721cd8ce84054a3
thiagocsr <76794619+thiagocsr@users.noreply.github.com>
2023-05-09 20:44:23 -0300

    Up

--- HackProjFinal/force-app/main/default/lwc/carouselDemo/carouselDemo.js
+++ HackProjFinal/force-app/main/default/lwc/carouselDemo/carouselDemo.js
@@ -8,24 +8,26 @@ export default class CarouselDemo extends LightningElement {
         // Declaração de uma variável chamada "carousel" que contém um array de objetos
         {
             id: "1",
-            header: "Oportunidade Única",
-            src: "https://img.freepik.com/vetores-gratis/ilustracao-de-design-plano-b2b_23-2149337639.jpg?size=626&ext=jpg&uid=R93830986&ga=GA1.2.460056409.1681601205&semt=robertav1_2_sidr",
-            href: "https://www.google.com.br/",
-            description: "Não deixe de contatar o Lead"
+            header: "Converta seu Lead em um Contato ou Oportunidade",
+            src: "https://t3.ftcdn.net/jpg/04/34/15/10/240_F_434151030_SjFza1XDaAWcE7kf4fX78r6H0qKAPxah.jpg",
+            description: "Saiba mais no site Salesforce"
+            /*https://img.freepik.com/vetores-gratis/ilustracao-de-design-plano-b2b_23-2149337639.jpg?size=626&ext=jpg&uid=R93830986&ga=GA1.2.460056409.1681601205&semt=robertav1_2_sidr*/
         },
         {
             id: "2",
             header: "Como atrair clientes",
-            src: "https://img.freepik.com/vetores-gratis/conceito-abstrato-de-marketing-de-engajamento_335657-3096.jpg?size=626&ext=jpg&uid=R93830986&ga=GA1.1.460056409.1681601205&semt=robertav1_2_sidr",
-            href: "https://www.google.com.br/",
-            description: "Leia nosso novo artigo no Blog"
+            src: "https://t3.ftcdn.net/jpg/04/85/22/48/240_F_485224821_qbCvGSP8wW6agQYZV7GSQ1wcwPi9gg9t.jpg",
+            href: "https://www.salesforce.com/br/resources/articles/prospeccao-de-clientes/",
+            description: "Saiba mais no site Salesforce"
+            /*https://img.freepik.com/vetores-gratis/conceito-abstrato-de-marketing-de-engajamento_335657-3096.jpg?size=626&ext=jpg&uid=R93830986&ga=GA1.1.460056409.1681601205&semt=robertav1_2_sidr*/
         },
         {
             id: "3",
-            header: "Saiba mais sobre funil de vendas",
-            src: "https://img.freepik.com/vetores-gratis/funil-de-vendas-geracao-de-leads-gestao-de-clientes-estrategia-de-marketing-elemento-de-design-plano-de-conversao-de-comercio-plano-de-venda-ilustracao-do-conceito-de-filtro-de-clientes_335657-2009.jpg?size=626&ext=jpg&uid=R93",
-            href: "https://www.google.com.br/",
-            description: "Leia nosso novo artigo no Blog"
+            header: "O que é funil de vendas?",
+            src: "https://t3.ftcdn.net/jpg/04/11/31/32/240_F_411313209_xopwlBdOw2vH6tQoHMQfd96RF7eit7Dg.jpg",
+            href: "https://www.salesforce.com/br/crm/funil-de-vendas/",
+            description: "Saiba mais no site Salesforce"
+            /*https://img.freepik.com/vetores-gratis/funil-de-vendas-geracao-de-leads-gestao-de-clientes-estrategia-de-marketing-elemento-de-design-plano-de-conversao-de-comercio-plano-de-venda-ilustracao-do-conceito-de-filtro-de-clientes_335657-2009.jpg?size=626&ext=jpg&uid=R93*/
         }
     ];
     handleImageClick(event) { 
